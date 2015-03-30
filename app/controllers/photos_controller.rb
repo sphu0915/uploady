52971918f55fec1978f8608992d7e3bd254ea093
sphu0915@gmail.com <sphu0915@gmail.com>
2015-03-30 19:23:34 -0400

    Add photo uploading and listing

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,27 @@
+class PhotosController < ApplicationController
+  def index
+  	@photos = Photo.all 
+
+  end
+
+  def new
+  	@photo = Photo.new
+  end
+
+  def create
+  	@photo = Photo.new(photo_params)
+
+  	if @photo.save
+  		redirect_to photos_path
+  	else
+  		render :new
+  	end
+  end
+
+  private
+
+  def photo_params
+  	params.require(:photo).permit(:filename, :caption)
+  end
+
+end
